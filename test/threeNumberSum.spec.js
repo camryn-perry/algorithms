62c848dbd58fdab3194b9dccfc4ebe70c5f31e35
Camryn Perry <camrynpaigeperry@gmail.com>
2020-03-02 11:32:58 -0500

    feat: added specs

--- test/threeNumberSum.spec.js
+++ test/threeNumberSum.spec.js
@@ -4,3 +4,15 @@ const threeNumberSum = require('../threeNumberSum/threeNumberSum')
 it('takes an array of positive integers', function(){
   chai.expect(threeNumberSum([1,2,3], 6)).to.deep.equal([[1,2,3]])
 })
+
+it('takes an array of negative and positive numbers and returns proper values', function(){
+  chai.expect(threeNumberSum([-8, 4, 6, 3, -2], 2)).to.deep.equal([[-8,4,6]])
+})
+
+it('returns all triplet arrays, in sorted order', function() {
+  chai.expect(threeNumberSum([12, 3, 1, 2, -6, 5, 0, -8, -1], 0)).to.deep.equal([
+    [-8, 3, 5],
+    [-6, 1, 5],
+    [-1, 0, 1],
+  ]);
+});
