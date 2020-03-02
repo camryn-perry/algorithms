b4e9b0e7a6be677682d9d069f838f8a5fd6cea80
Camryn Perry <camrynpaigeperry@gmail.com>
2020-03-02 11:45:26 -0500

    feat: passing specs, will add more

--- threeNumberSum/threeNumberSum.js
+++ threeNumberSum/threeNumberSum.js
@@ -1,7 +1,25 @@
-function threeNumberSum(array, target){
+function threeNumberSum(array, targetSum){
   const allSums = [];
+  //sort array in order to use pointers
+  array.sort((a,b) => a - b);
   for(let i = 0; i < array.length; i++){
-
+    let current = array[i];
+    let left = i + 1;
+    let right = array.length -1;
+    //while the left pointer is less than right pointer, add up values in array
+    //to current value
+    while(left < right){
+      let currentSum = current + array[left] + array[right];
+      if(currentSum === targetSum){
+        allSums.push([current, array[left], array[right]]);
+        left++;
+        right--;
+      }else if(currentSum < targetSum){
+        left++
+      }else if(currentSum > targetSum){
+        right--;
+      }
+    }
   }
   return allSums;
 }
