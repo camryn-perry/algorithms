3c1a86e39b6abdf2c1ba17c44db4629f3df49f21
Camryn Perry <camrynpaigeperry@gmail.com>
2020-03-06 11:19:43 -0500

    feat: added more specs

--- nthFibonacci/nthFibonacci.js
+++ nthFibonacci/nthFibonacci.js
@@ -1,12 +1,18 @@
 //using recursion
+// function nthFibonacci(num){
+//   //establish base cases
+//   if(num <= 1){
+//     return 0;
+//   }if(num === 2){
+//     return 1;
+//   }else{
+//     return nthFibonacci(num - 1) + nthFibonacci(num - 2)
+//   }
+// }
+
+//dynamic programming solution
 function nthFibonacci(num){
-  //establish base cases
-  if(num <= 1){
-    return 0;
-  }if(num === 2){
-    return 1;
-  }else{
-    return nthFibonacci(num - 1) + nthFibonacci(num - 2)
-  }
+  const fibNums = {};
+
 }
 module.exports = nthFibonacci;
--- test/nthFibonacci.spec.js
+++ test/nthFibonacci.spec.js
@@ -1,10 +1,50 @@
 const chai = require('chai');
 const nthFibonacci = require('../nthFibonacci/nthFibonacci')
 
-it('test case for first fib', function() {
+it('Test num 1', function() {
   chai.expect(nthFibonacci(1)).to.deep.equal(0);
 });
 
-it('test case for second fib', function() {
+it('Test num 2', function() {
   chai.expect(nthFibonacci(2)).to.deep.equal(1);
+});
+
+it('Test num 3', function() {
+  chai.expect(nthFibonacci(3)).to.deep.equal(1);
+});
+
+it('Test num 4', function() {
+  chai.expect(nthFibonacci(4)).to.deep.equal(2);
+});
+
+it('Test num 5', function() {
+  chai.expect(nthFibonacci(5)).to.deep.equal(3);
+});
+
+it('Test num 6', function() {
+  chai.expect(nthFibonacci(6)).to.deep.equal(5);
+});
+
+it('Test num 7', function() {
+  chai.expect(nthFibonacci(7)).to.deep.equal(8);
+});
+
+it('Test num 8', function() {
+  chai.expect(nthFibonacci(8)).to.deep.equal(13);
+});
+
+it('Test num 9', function() {
+  chai.expect(nthFibonacci(9)).to.deep.equal(21);
+});
+
+it('Test num 10', function() {
+  chai.expect(nthFibonacci(10)).to.deep.equal(34);
+});
+
+it('Test num 11', function() {
+  chai.expect(nthFibonacci(11)).to.deep.equal(55);
+});
+
+it('Test num 12', function() {
+  chai.expect(nthFibonacci(12)).to.deep.equal(89);
 });
